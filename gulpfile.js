7f24f1d9b63d4a058aff1430460048212b41838f
zarias2020 <zarias@yandex.ru>
2020-05-01 09:59:47 +0300

    added header, slider, services

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,7 @@ gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
+        'node_modules/swiper/css/swiper.css',
         'node_modules/magnific-popup/dist/magnific-popup.css'
     ])
         .pipe(concat('libs.min.css'))
@@ -34,6 +35,7 @@ gulp.task('style', function(){
 gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/swiper/js/swiper.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
     ])
         .pipe(concat('libs.min.js'))
